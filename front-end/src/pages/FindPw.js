e635a165db4334ad0c337df60130c0d6019a26d2
scriptfetish <byroad-ricotta0r@icloud.com>
2023-09-01 14:14:44 +0900

    [23.09.01][FE-User] 유저페이지

---
+++ front-end/src/pages/FindPw.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+
+import { Link } from 'react-router-dom';
+import './styles/style.css';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const FindPw = () => {
+
+  const [findPwtext, setText] = useState('');
+  const [findPwtext1, setText1] = useState('');
+  const [findPwtext2, setText2] = useState('');
+  
+  const handleTextChange = (event) => {
+    const newValue = event.target.value;
+    if (/^[A-Za-z0-9@.\-_]*$/.test(newValue) || newValue === '') {
+      setText(newValue);
+    }
+  };
+
+  const handleTextChange1 = (event) => {
+    const newValue = event.target.value;
+    if (/^[0-9]*$/.test(newValue) || newValue === '') {
+      setText1(newValue);
+    }
+  };
+
+  const handleTextChange2 = (event) => {
+    const newValue = event.target.value;
+    if (/^[0-9]*$/.test(newValue) || newValue === '') {
+      setText1(newValue);
+    }
+  };
+
+  const [open, setOpen] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0); // 스크롤 위치 저장
+  const [isTimerActive1, setIsTimerActive1] = useState(false);
+  const [remainingTime1, setRemainingTime1] = useState(null);
+  const [timerId1, setTimerId1] = useState(null);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '1px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  const handleOpen = () => {
+    setScrollPosition(window.scrollY); // 모달 열릴 때 스크롤 위치 저장
+    setOpen(true);
+    setIsTimerActive1(true);
+    const newTimerId = startTimer();
+    setTimerId1(newTimerId);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    window.scrollTo(0, scrollPosition); // 모달 닫힐 때 스크롤 위치 복원
+  };
+
+  const startTimer = () => {
+    const newTimerId = setInterval(() => {
+      setRemainingTime1(prevTime => prevTime - 1);
+    }, 1000);
+
+    return newTimerId;
+  };
+
+  const formatTime = time => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden'; // 모달 열릴 때 스크롤 막기
+    } else {
+      document.body.style.overflow = 'auto'; // 모달 닫힐 때 스크롤 복원
+      window.scrollTo(0, scrollPosition); // 스크롤 위치 복원
+    }
+  }, [open]);
+  
+
+  useEffect(() => {
+    let timerInterval;
+  
+    if (open && !timerInterval) {
+      setIsTimerActive1(true);
+      setRemainingTime1(180); // 3분으로 설정
+    }
+  
+    return () => {
+      if (timerInterval) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+      }
+    };
+  }, [open]);
+
+
+
+  return (
+  
+    <div className="element-user-findpw">
+      <div className="div">
+        <input type='text' value={findPwtext} onChange={handleTextChange} className="rectangle" />
+        <input type='text' value={findPwtext1} onChange={handleTextChange1} className="rectangle-2" />
+        <div className="text-wrapper">휴대폰 번호 입력</div>
+        <input type='text' value={findPwtext2} onChange={handleTextChange2} className="rectangle-3" />
+        <div className="text-wrapper-2">ID 또는 이메일</div>
+        <div className="text-wrapper-3">인증번호 입력<span className='time_auth'>{formatTime(remainingTime1)}</span></div>
+        <Link to="/findpw1"><div className="overlap-group">
+          <div className="rectangle-4" />
+          <div className="text-wrapper-4">인증 확인</div>
+        </div></Link>
+        <Button onClick={handleOpen}><div className="overlap">
+          <div className="text-wrapper-5">인증번호 받기</div>
+        </div></Button>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            인증 번호가 발송 되었습니다.
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            발송된 번호를 입력해주세요.
+          </Typography>
+        </Box>
+      </Modal>
+      </div>
+    </div>
+    );
+
+  
+  };
+  
+  export default FindPw;
