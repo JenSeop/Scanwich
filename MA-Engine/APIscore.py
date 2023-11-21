568124279f0ffecace1ad58e96cd4d9358edb311
Yujeong <yjeong34@duksung.ac.kr>
2023-11-21 14:33:28 +0900

    [23.11.21][MA] Score output for each category

--- MA-Engine/APIscore.py
+++ MA-Engine/APIscore.py
@@ -1,7 +1,7 @@
 import json
 
 # JSON 파일 경로를 직접 지정
-json_file_path = 'Sample.json'
+json_file_path = 'APKtoJson.json'
 
 # JSON 파일을 읽어와서 파싱
 with open(json_file_path, 'r') as json_file:
@@ -22,6 +22,7 @@ api_categories = {
 # 각 API 카테고리의 호출 횟수 저장할 딕셔너리 초기화
 category_counts = {category: {api: 0 for api in apis} for category, apis in api_categories.items()}
 
+
 # 각 클래스의 메서드를 확인하여 특정 API 호출 횟수를 세기
 for class_info in json_data['Classes']:
     methods = class_info.get('method', [])
@@ -32,32 +33,52 @@ for class_info in json_data['Classes']:
             if api_name in apis:
                 category_counts[category][api_name] += 1
 
-# 결과 출력
+# 결과 출력 및 딕셔너리에 반영
 for category, api_count in category_counts.items():
     print(f"{category} API:")
     for api, count in api_count.items():
-        # 특정 API에 대한 추가 조건에 따라 곱셈 적용
+        # API 위험도에 따른 가중치 곱셈 및 딕셔너리에 반영함
         if api == 'getDeviceId':
-            count *= 4
+            category_counts[category][api] *= 4
         elif api == 'getSimSerialNumber':
-            count *= 6
+            category_counts[category][api] *= 6
         elif api == 'getSubscriberId':
-            count *= 5
+            category_counts[category][api] *= 5
         elif api == 'sendTextMessage' or api == 'getMessageBody':
-            count *= 9
+            category_counts[category][api] *= 9
         elif api == 'delete' or api == 'createFromPdu':
-            count *= 3
+            category_counts[category][api] *= 3
         elif api == 'getLineNumber':
-            count *= 6
+            category_counts[category][api] *= 6
         elif api == 'getRunningTasks':
-            count *= 7
+            category_counts[category][api] *= 7
         elif api in ['getSimOperator', 'getSimOperatorName']:
-            count *= 6
+            category_counts[category][api] *= 6
         elif api in ['connect']:
-            count *= 3
+            category_counts[category][api] *= 3
         elif api in ['notify', 'killBackgroundProcesses', 'registerContentObserver']:
-            count *= 3
+            category_counts[category][api] *= 3
         elif api in ['encode', 'getURL']:
-            count *= 3
-        print(f'The API "{api}" was called {count} times.')
+            category_counts[category][api] *= 3
+        print(f'The API "{api}" was called {category_counts[category][api]} times.')
     print()
+
+
+# 각 카테고리에 속하는 값들을 모두 더함
+device_sum = sum(category_counts['Privacy'].values())
+print(f"The sum of values in 'Privacy' category is: {device_sum}")
+
+device_sum = sum(category_counts['SMS'].values())
+print(f"The sum of values in 'SMS' category is: {device_sum}")
+
+device_sum = sum(category_counts['Linux'].values())
+print(f"The sum of values in 'Linux' category is: {device_sum}")
+
+device_sum = sum(category_counts['FileAccess'].values())
+print(f"The sum of values in 'FileAccess' category is: {device_sum}")
+
+device_sum = sum(category_counts['Device'].values())
+print(f"The sum of values in 'Device' category is: {device_sum}")
+
+device_sum = sum(category_counts['Network'].values())
+print(f"The sum of values in 'Network' category is: {device_sum}")
