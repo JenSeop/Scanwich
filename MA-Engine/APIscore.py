528550a291cf0324b41e297bf58ccfe806cdea46
Yujeong <yjeong34@duksung.ac.kr>
2023-11-23 19:51:36 +0900

    [23.11.23][MA] APIscore Code functionalization

--- MA-Engine/APIscore.py
+++ MA-Engine/APIscore.py
@@ -1,108 +1,71 @@
 import json
 
-def apply_threat_score(api_name, category_counts):
+# API에 대한 위협 가중치를 리턴하는 함수.
+def apply_threat_score(api_name):
+    # API에 대한 위협 가중치를 정의한 딕셔너리.
     threat_scores = {
-        'getDeviceId': 4,
-        'getSimSerialNumber': 6,
-        'getSubscriberId': 5,
-        'sendTextMessage': 9,
-        'getMessageBody': 9,
-        'delete': 3,
-        'createFromPdu': 3,
-        'getLineNumber': 6,
-        'getRunningTasks': 7,
-        'getSimOperator': 6,
-        'getSimOperatorName': 6,
-        'connect': 3,
-        'notify': 3,
-        'killBackgroundProcesses': 3,
-        'registerContentObserver': 3,
-        'encode': 3,
-        'getURL': 3,
+        'getDeviceId': 4, 'getSimSerialNumber': 6, 'getSubscriberId': 5,
+        'sendTextMessage': 9, 'getMessageBody': 9, 'delete': 3, 'createFromPdu': 3,
+        'getLineNumber': 6, 'getRunningTasks': 7, 'getSimOperator': 6,
+        'getSimOperatorName': 6, 'connect': 3, 'notify': 3,
+        'killBackgroundProcesses': 3, 'registerContentObserver': 3,
+        'encode': 3, 'getURL': 3,
     }
-    return threat_scores.get(api_name, 1)  # Default score is 1 if not found
+    # API 이름에 해당하는 위협 가중치를 반환함. 없으면 기본값 1 반환
+    return threat_scores.get(api_name, 1)
 
-def print_threat_scores(category_counts):
+def calculate_category_score(category_counts):
+    # 각 카테고리에 대한 API 점수 합이 100을 넘지 않도록 하는 함수
+    return {category: min(sum(api_count.values()), 100) for category, api_count in category_counts.items()}
+
+def calculate_threat_scores(category_counts):
+    # 카테고리별 API 카운트에 위협 가중치를 곱하는 함수
     for category, api_count in category_counts.items():
-        print(f"{category} API:")
         for api, count in api_count.items():
-            threat_score = apply_threat_score(api, category_counts[category])
+            # apply_threat_score 함수를 통해 API 이름에 맞는 위협 가중치를 가져옴
+            threat_score = apply_threat_score(api)
+            # 기존 API 호출 횟수에 위협 가중치를 곱하여 category_counts 딕셔너리에 반영함.
             category_counts[category][api] *= threat_score
-            print(f'The Threat score for "{api}" is {category_counts[category][api]}')
-        print()
+    return category_counts
 
-def print_category_scores(category, category_counts):
-    device_sum = sum(category_counts[category].values())
-    category_score = min(device_sum, 100)
-    print(f"'{category}' category Score is {category_score}")
+def main():
+    # Json 파일 경로를 직접 설정함.
+    json_file_path = 'APKtoJson.json'
 
-# JSON 파일 경로를 직접 지정
-json_file_path = 'APKtoJson.json'
+    with open(json_file_path, 'r') as json_file:
+        # JSON 파일을 읽어서 데이터를 json_data에 저장함.
+        json_data = json.load(json_file)
 
-# JSON 파일을 읽어와서 파싱
-with open(json_file_path, 'r') as json_file:
-    json_data = json.load(json_file)
+    # 각 카테고리별로 API 목록을 정의함.
+    api_categories = {
+        'Privacy': ['getDeviceId', 'getSimSerialNumber', 'getSubscriberId'],
+        'SMS': ['sendTextMessage', 'getMessageBody'],
+        'Linux': ['su', 'du', 'gzip', 'mkdir', 'chmod', 'chown', 'cp'],
+        'FileAccess': ['delete', 'createFromPdu'],
+        'Device': ['getLineNumber', 'getLatitude', 'getLongitude', 'getRunningTasks',
+                   'getSimOperator', 'getSimOperatorName', 'connect', 'notify',
+                   'killBackgroundProcesses', 'registerContentObserver'],
+        'Network': ['encode', 'getURL']
+    }
 
-# 각 API 호출 횟수를 저장할 딕셔너리
-api_categories = {
-    'Privacy': ['getDeviceId', 'getSimSerialNumber', 'getSubscriberId'],
-    'SMS': ['sendTextMessage', 'getMessageBody'],
-    'Linux': ['su', 'du', 'gzip', 'mkdir', 'chmod', 'chown', 'cp'],
-    'FileAccess': ['delete', 'createFromPdu'],
-    'Device': ['getLineNumber', 'getLatitude', 'getLongitude', 'getRunningTasks',
-               'getSimOperator', 'getSimOperatorName', 'connect', 'notify',
-               'killBackgroundProcesses', 'registerContentObserver'],
-    'Network': ['encode', 'getURL']
-}
+    # 각 카테고리에 속하는 API들의 호출 횟수를 0으로 초기화함.
+    category_counts = {category: {api: 0 for api in apis} for category, apis in api_categories.items()}
 
-# 각 API 카테고리의 호출 횟수 저장할 딕셔너리 초기화. 각 key를 0으로 초기화.
-category_counts = {category: {api: 0 for api in apis} for category, apis in api_categories.items()}
+    # JSON 데이터에서 클래스 정보를 추출하고 API가 호출될 때마다 API를 카테고리에 매핑하여 카운트를 1씩 증가시킴.
+    for class_info in json_data['Classes']:
+        methods = class_info.get('method', [])
+        for method in methods:
+            api_name = method.get('name')
+            for category, apis in api_categories.items():
+                if api_name in apis:
+                    category_counts[category][api_name] += 1
 
-# 각 클래스의 메서드를 확인하여 특정 API 호출 횟수를 세기
-for class_info in json_data['Classes']:
-    methods = class_info.get('method', [])
-    for method in methods:
-        api_name = method.get('name')
-        # 어떤 카테고리에 속하는지 확인하고 호출 횟수 증가
-        for category, apis in api_categories.items():
-            if api_name in apis:
-                category_counts[category][api_name] += 1
+    # 위협 점수를 적용한 API 카운트를 계산함
+    modified_category_counts = calculate_threat_scores(category_counts)
 
-# api 호출 횟수를 출력하고 위험도에 따라 가중치를 곱함.
-print_threat_scores(category_counts)
+    # 각 카테고리에 대한 총 점수를 계산하여 출력함
+    category_scores = calculate_category_score(modified_category_counts)
+    print("Category Score Sum:", category_scores)
 
-# Privacy 카테고리에 속하는 값들을 모두 더해서 Privacy_score에 저장
-privacy_category = 'Privacy'
-privacy_sum = sum(category_counts[privacy_category].values())
-Privacy_score = min(privacy_sum, 100)
-print(f"'{privacy_category}' category Score is {Privacy_score}")
-
-# SMS 카테고리에 속하는 값들을 모두 더해서 SMS_score에 저장
-sms_category = 'SMS'
-sms_sum = sum(category_counts[sms_category].values())
-SMS_score = min(sms_sum, 100)
-print(f"'{sms_category}' category Score is {SMS_score}")
-
-# Linux 카테고리에 속하는 값들을 모두 더해서 Linux_score 저장
-linux_category = 'Linux'
-linux_sum = sum(category_counts[linux_category].values())
-Linux_score = min(linux_sum, 100)
-print(f"'{linux_category}' category Score is {Linux_score}")
-
-# FileAccess 카테고리에 속하는 값들을 모두 더해서 Privacy_score에 저장
-fileAcess_category = 'FileAccess'
-fileAcess_sum = sum(category_counts[fileAcess_category].values())
-FileAcess_score = min(fileAcess_sum, 100)
-print(f"'{fileAcess_category}' category Score is {FileAcess_score}")
-
-# Device 카테고리에 속하는 값들을 모두 더해서 Device_score에 저장
-device_category = 'Device'
-device_sum = sum(category_counts[device_category].values())
-Device_score = min(device_sum, 100)
-print(f"'{device_category}' category Score is {Device_score}")
-
-# Network 카테고리에 속하는 값들을 모두 더해서 Network_score 저장
-network_category = 'Network'
-network_sum = sum(category_counts[network_category].values())
-Network_score = min(network_sum, 100)
-print(f"'{network_category}' category Score is {Network_score}")
+if __name__ == "__main__":
+    main()
